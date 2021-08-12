cffc88535dac4693b4b6571832da291eebfb5dd1
helenzzq <helenzhu0411@gmail.com>
2021-08-12 17:04:09 -0400

    Init repo

---
+++ portofolioManager-IanB/src/main/java/com/citi/training/portofolioManagerIanB/PortofolioManagerIanBApplication.java
@@ -0,0 +1,13 @@
+package com.citi.training.portofolioManagerIanB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortofolioManagerIanBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortofolioManagerIanBApplication.class, args);
+	}
+
+}
