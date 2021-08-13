34aa30b7fd3830d6a77c94b795517e7c3dd234d5
Yanhan Liu <yanhan.liu@mail.mcgill.ca>
2021-08-13 13:25:57 -0400

    refactor

--- portfolioManager-IanB/src/main/java/com/citi/training/portofolioManagerIanB/services/PortfolioManagerServiceImpl.java
+++ portfolioManager-IanB/src/main/java/com/citi/training/portofolioManagerIanB/services/PortfolioManagerServiceImpl.java
@@ -3,13 +3,12 @@ package com.citi.training.portofolioManagerIanB.services;
 import com.citi.training.portofolioManagerIanB.entities.Investments;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.citi.training.portofolioManagerIanB.repo.PortfolioManagerReposoitory;
 
 import java.util.Collection;
 
-public class PortfolioManagerServiceImpl implements PortfolioManagerService {
+public class PortfolioManagerServiceImpl<PortfolioManagerRepository> implements PortfolioManagerService {
 	@Autowired
-    private PortfolioManagerReposoitory portfolioManagerRepository;
+    private PortfolioManagerRepository portfolioManagerRepository;
 
     @Override
     public Collection<Investments> getAllInvestments() {
