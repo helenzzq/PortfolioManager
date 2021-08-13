ff1a70588e9acb8c08726fbefc31d38794da2498
helenzzq <helenzhu0411@gmail.com>
2021-08-13 14:30:35 -0400

    fix class error

--- portfolioManager-IanB/src/main/java/com/citi/training/portofolioManagerIanB/services/PortfolioManagerServiceImpl.java
+++ portfolioManager-IanB/src/main/java/com/citi/training/portofolioManagerIanB/services/PortfolioManagerServiceImpl.java
@@ -1,19 +1,19 @@
 package com.citi.training.portofolioManagerIanB.services;
 
 import com.citi.training.portofolioManagerIanB.entities.Investments;
+import com.citi.training.portofolioManagerIanB.repo.PortfolioManagerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
 
 import java.util.Collection;
 
-public class PortfolioManagerServiceImpl<PortfolioManagerRepository> implements PortfolioManagerService {
+public class PortfolioManagerServiceImpl implements PortfolioManagerService {
 	@Autowired
     private PortfolioManagerRepository portfolioManagerRepository;
 
     @Override
     public Collection<Investments> getAllInvestments() {
-//    return PortfolioManagerReposoitory.findAll();
-        return null;
+    return portfolioManagerRepository.findAll();
     }
 
 }
