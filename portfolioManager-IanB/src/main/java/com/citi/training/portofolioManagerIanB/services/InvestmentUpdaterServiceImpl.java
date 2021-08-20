faf345ae945c7397d71f8af5f1bcfd35f8ea100c
helenzzq <helenzhu0411@gmail.com>
2021-08-20 11:43:31 -0400

    Create and implement etf downloader

--- portfolioManager-IanB/src/main/java/com/citi/training/portofolioManagerIanB/services/InvestmentUpdaterServiceImpl.java
+++ portfolioManager-IanB/src/main/java/com/citi/training/portofolioManagerIanB/services/InvestmentUpdaterServiceImpl.java
@@ -1,13 +1,14 @@
 package com.citi.training.portofolioManagerIanB.services;
 
 import com.citi.training.portofolioManagerIanB.repo.PortfolioManagerRepository;
-import com.citi.training.portofolioManagerIanB.services.yahooFinanceDownloader.MarkerMovers;
-import com.citi.training.portofolioManagerIanB.services.yahooFinanceDownloader.StockDownloader;
+import com.citi.training.portofolioManagerIanB.services.marketData.MarkerMovers;
+import com.citi.training.portofolioManagerIanB.services.marketData.StockDownloader;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
+
 @Service
 public class InvestmentUpdaterServiceImpl implements InvestmentsUpdaterServices {
     @Autowired
@@ -16,15 +17,15 @@ public class InvestmentUpdaterServiceImpl implements InvestmentsUpdaterServices 
     private MarkerMovers marketMovers;
     private StockDownloader stockDownloader;
 
-    public void initDownloader() throws Exception {
-        String HOST = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-summary";
-        String API_KEY = "bdd16c27d4mshd5948dee3978ee3p1ea11fjsncc40f6d4e8ed";
-        String API_HOST = "apidojo-yahoo-finance-v1.p.rapidapi.com";
-        String MKT_MOVER_QUERY = "region=US&lang=en-US&count=5&start=0";
-        stockDownloader = new StockDownloader(HOST, API_HOST, API_KEY);
-        marketMovers = new MarkerMovers(HOST, API_HOST, API_KEY);
-        marketMovers.retrieveData(MKT_MOVER_QUERY);
-    }
+//    public void initDownloader() throws Exception {
+
+//
+//
+//        String MKT_MOVER_QUERY = "region=US&lang=en-US&count=5&start=0";
+//
+//        marketMovers = new MarkerMovers(HOST,MKT_MOVER_QUERY);
+//        marketMovers.retrieveData();
+//    }
 
     public HashMap<Integer, String> getDailyGainers() {
         return marketMovers.getGainers();
@@ -35,7 +36,8 @@ public class InvestmentUpdaterServiceImpl implements InvestmentsUpdaterServices 
     }
 
     public Double getStockPrice(String symbol) throws UnirestException {
-        stockDownloader.retrieveData(symbol);
+        String HOST = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-summary";
+        StockDownloader stockDownloader = new StockDownloader(HOST, symbol);
         return stockDownloader.getPrice();
     }
 }
