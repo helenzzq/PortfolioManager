cde3ad6d5e08398abe0abcd7204005155c86ceaa
ilker-h <ilker.hadzhalaran@gmail.com>
2021-08-25 05:31:42 +0000

    get Angular Material working

--- portfolioManager-IanB/angular-frontend/src/app/app.module.ts
+++ portfolioManager-IanB/angular-frontend/src/app/app.module.ts
@@ -1,8 +1,15 @@
+// Core Angular modules
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Components within the application
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// Angular Material modules
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
