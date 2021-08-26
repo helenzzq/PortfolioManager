1a1dabde5c76f6da7ed07ca13b1dea91d1d1d63a
Yanhan Liu <yanhan.liu@mail.mcgill.ca>
2021-08-26 13:29:53 -0400

    update

--- portfolioManager-IanB/angular-frontend/src/app/dashboard/portfolio/portfolio.component.ts
+++ portfolioManager-IanB/angular-frontend/src/app/dashboard/portfolio/portfolio.component.ts
@@ -4,13 +4,14 @@ export interface portfolioTableRow {
   ticker: string;
   marketPrice: number;
   percentRetained: number;
+  grouped: string;
 }
 
  // portfolio table
  const PORTFOLIO_TABLE_DATA: portfolioTableRow[] = [
-  { ticker: 'ABCD', marketPrice: 112.70, percentRetained: 3.54},
-  { ticker: 'ALOHA', marketPrice: 80.96, percentRetained: 4.06},
-  { ticker: 'LOTR', marketPrice: 48.33, percentRetained: -1.23}
+  { ticker: 'ABCD', marketPrice: 112.70, percentRetained: 3.54, grouped: 'a'},
+  { ticker: 'ALOHA', marketPrice: 80.96, percentRetained: 4.06, grouped: 'a'},
+  { ticker: 'LOTR', marketPrice: 48.33, percentRetained: -1.23, grouped: 'a'}
 ];
 
 @Component({
@@ -22,5 +23,6 @@ export interface portfolioTableRow {
 export class PortfolioComponent {
   constructor() {}
   displayedPortfolioColumns: string[] = ['ticker', 'marketPrice', 'percentRetained'];
+  groupedColumns: string[] = ['grouped'];
   portfolioDataSource = PORTFOLIO_TABLE_DATA;
 }
