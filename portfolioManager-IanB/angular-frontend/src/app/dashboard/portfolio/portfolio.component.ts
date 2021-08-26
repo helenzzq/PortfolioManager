7bae077a09686fd61dc242ef0a21ef268db62d8f
Yanhan Liu <yanhan.liu@mail.mcgill.ca>
2021-08-26 11:12:20 -0400

    portfolio table

--- portfolioManager-IanB/angular-frontend/src/app/dashboard/portfolio/portfolio.component.ts
+++ portfolioManager-IanB/angular-frontend/src/app/dashboard/portfolio/portfolio.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+
+export interface portfolioTableRow {
+  ticker: string;
+  marketPrice: number;
+  percentRetained: number;
+}
+
+ // portfolio table
+ const PORTFOLIO_TABLE_DATA: portfolioTableRow[] = [
+  { ticker: 'ABCD', marketPrice: 112.70, percentRetained: 3.54},
+  { ticker: 'ALOHA', marketPrice: 80.96, percentRetained: 4.06},
+  { ticker: 'LOTR', marketPrice: 48.33, percentRetained: -1.23}
+];
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.css']
 })
-export class PortfolioComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class PortfolioComponent {
+  constructor() {}
+  displayedPortfolioColumns: string[] = ['ticker', 'marketPrice', 'percentRetained'];
+  portfolioDataSource = PORTFOLIO_TABLE_DATA;
 }
