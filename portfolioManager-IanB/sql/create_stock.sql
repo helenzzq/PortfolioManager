eaf014afe714055821428a97623cf14464b25860
helenzzq <helenzhu0411@gmail.com>
2021-08-26 23:01:28 -0400

    Add more data

--- portfolioManager-IanB/sql/create_stock.sql
+++ portfolioManager-IanB/sql/create_stock.sql
@@ -10,6 +10,6 @@ CREATE TABLE `conygre`.`stock` (
                                          `percent_retained` DECIMAL(10,2) NOT NULL,
                                          `percent_port` DECIMAL(10,2) NOT NULL,
                                          PRIMARY KEY (`ticker`));
-INSERT INTO `conygre`.`stock` (`ticker`, `portfolio_id`, `currency`, `quantity`, `cost_per_share`, `market_price`, `market_value`, `profit_and_loss`, `percent_retained`, `percent_port`) VALUES ('BILI', '1', 'USD', '13', '80', '78', '1060', '-26', '1.99', '15');
-INSERT INTO `conygre`.`stock` (`ticker`, `portfolio_id`, `currency`, `quantity`, `cost_per_share`, `market_price`, `market_value`, `profit_and_loss`, `percent_retained`, `percent_port`) VALUES ('GME', '1', 'USD', '12', '180', '210.29', '2654', '363', '16.8', '26');
+INSERT INTO `conygre`.`stock` (`ticker`, `portfolio_id`, `currency`, `quantity`, `cost_per_share`, `market_price`, `market_value`, `profit_and_loss`, `percent_retained`, `percent_port`) VALUES ('BILI', '1', 'USD', '14', '80', '78', '1060', '-28', '1.99', '15');
+INSERT INTO `conygre`.`stock` (`ticker`, `portfolio_id`, `currency`, `quantity`, `cost_per_share`, `market_price`, `market_value`, `profit_and_loss`, `percent_retained`, `percent_port`) VALUES ('GME', '1', 'USD', '17', '180', '210.29', '3754', '363', '16.8', '26');
 INSERT INTO `conygre`.`stock` (`ticker`, `portfolio_id`, `currency`, `quantity`, `cost_per_share`, `market_price`, `market_value`, `profit_and_loss`, `percent_retained`, `percent_port`) VALUES ('SU', '1', 'USD', '59', '17.66', '18.66', '1101', '59', '5.64', '9');
